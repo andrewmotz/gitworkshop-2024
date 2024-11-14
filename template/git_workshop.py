b5d898857c19fbb9a7663f8e61540f0e0dabd5d6
Andrew Motz <andrewmozza555@gmail.com>
2024-11-13 20:37:13 -0800

    fix: update year in current_year

--- template/git_workshop.py
+++ template/git_workshop.py
@@ -1,19 +1,20 @@
 # Edit this file
 
-def first_ten_even_integers():
-    result = []
-    for i in range(1,50):
-        if i % 2 == 0:
-            result.append(i)
-    return result
+# def first_n_even_integers(n):
+#     result = []
+#     for i in range(1,n*2):
+#         if i % 1 == 0:
+#             result.append(i)
+#     return result
 
 
-def current_year():
-    return 2022
+# def current_year():
+#     return 2023
 
 def main():
-    print(f"Welcome to the {current_year()} git workshop!")
-    print(f"The first 10 even integers are: {first_ten_even_integers()}")
+    # print(f"Welcome to the {current_year()} git workshop!")
+    # print(f"The first 10 even integers are: {first_n_even_integers(10)}")
+    pass
 
 if __name__ == "__main__":
     main()
--- template/tests/test_main.py
+++ template/tests/test_main.py
@@ -1,9 +1,20 @@
 # DO NOT EDIT ME
 
-from ..git_workshop import current_year, first_ten_even_integers
+from .. import git_workshop as ws
+import pytest
 
+# current_year tests
+test_function="current_year"
+@pytest.mark.skipif(not hasattr(ws, test_function), reason=test_function)
 def test_current_year():
-    assert current_year == 2024
+    assert ws.current_year() == 2024
 
+# first_n_even_integers tests
+test_function="first_n_even_integers"
+@pytest.mark.skipif(not hasattr(ws, test_function), reason=test_function)
 def test_first_ten_even_integers():
-    assert first_ten_even_integers() == [2,4,6,8,10,12,14,16,18,20]
+    assert ws.first_n_even_integers(10) == [2,4,6,8,10,12,14,16,18,20]
+
+@pytest.mark.skipif(not hasattr(ws, test_function), reason=test_function)
+def test_first_2_even_integers():
+    assert ws.first_n_even_integers(2) == [2,4]
