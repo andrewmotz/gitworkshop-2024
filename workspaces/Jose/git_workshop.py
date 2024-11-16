53f18b85147a5b9b2a2542a577e20e2eb3b10caa
Andrew Motz <andrewmozza555@gmail.com>
2024-11-16 13:03:16 -0800

    Add generated files from generate.py

---
+++ workspaces/Jose/git_workshop.py
@@ -0,0 +1,21 @@
+# Edit this file
+
+# should return [1,1,2,3,5... and so on
+# def first_n_fibonacci(n):
+#     pass
+
+# if n=5, should return 2,4,6,8,10
+# def first_n_even_integers(n):
+#    pass
+
+# def current_year():
+#     return 2023
+
+def main():
+    # print(f"Welcome to the {current_year()} git workshop!")
+    # print(f"The first 10 even integers are: {first_n_even_integers(10)}")
+    # print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
+    pass
+
+if __name__ == "__main__":
+    main()
