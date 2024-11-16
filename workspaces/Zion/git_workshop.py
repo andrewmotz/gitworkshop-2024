40b450fad8b6957e72653713cf0fc2521c62e025
Zion Chong <zion2027@gmail.com>
2024-11-16 13:26:51 -0800

    Uncomment and made it more happy!

--- workspaces/Zion/git_workshop.py
+++ workspaces/Zion/git_workshop.py
@@ -8,14 +8,17 @@
 # def first_n_even_integers(n):
 #    pass
 
-# def current_year_workshop():
-#     return 2023
+
+
+def current_year_workshop():
+    return 2024
 
 def main():
-    # print(f"Welcome to the {current_year_workshop()} git workshop!")
+    print(f"Welcome to the {current_year_workshop()} git workshop! I am having fun learning git!")
+
     # print(f"The first 10 even integers are: {first_n_even_integers(10)}")
     # print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
-    pass
+    #pass
 
 if __name__ == "__main__":
     main()
