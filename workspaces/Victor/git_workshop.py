4c3d274215f13bfd4586838cff46093d37fa0763
Victor Tom <vjrtom@gmail.com>
2024-11-16 14:26:00 -0800

    commented things

--- workspaces/Victor/git_workshop.py
+++ workspaces/Victor/git_workshop.py
@@ -13,7 +13,7 @@ def current_year():
 
 def main():
     print(f"Welcome to the {current_year()} git workshop!")
-    print(f"The first 10 even integers are: {first_n_even_integers(10)}")
+    # print(f"The first 10 even integers are: {first_n_even_integers(10)}")
     # print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
     #pass
 
