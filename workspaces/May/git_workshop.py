2111807d74523e64ef3c0392e7065e6bb9da9d90
Thu Tran <anhthutr.185@gmail.com>
2024-11-16 14:25:40 -0800

    Uncommentted some extra lines.

--- workspaces/May/git_workshop.py
+++ workspaces/May/git_workshop.py
@@ -8,13 +8,13 @@
 # def first_n_even_integers(n):
 #    pass
 
- def current_year():
+def current_year():
      return 2023
 
 def main():
     print(f"Welcome to the {current_year()} git workshop!")
-    # print(f"The first 10 even integers are: {first_n_even_integers(10)}")
-    # print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
+    print(f"The first 10 even integers are: {first_n_even_integers(10)}")
+    print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
     pass
 
 if __name__ == "__main__":
