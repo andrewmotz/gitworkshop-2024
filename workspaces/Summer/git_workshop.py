31e78b4e942ce5ec8b447b52c7c8f0984505bc39
Samar <130741241+SUMMERxKx@users.noreply.github.com>
2024-11-16 13:31:11 -0800

    uncommented the chagne

--- workspaces/Summer/git_workshop.py
+++ workspaces/Summer/git_workshop.py
@@ -7,14 +7,13 @@
 # if n=5, should return 2,4,6,8,10
 # def first_n_even_integers(n):
 #    pass
-
-# def current_year():
-#     return 2023
+def current_year():
+     return 2023
 
 def main():
-    # print(f"Welcome to the {current_year()} git workshop!")
-    # print(f"The first 10 even integers are: {first_n_even_integers(10)}")
-    # print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
+    print(f"Welcome to the {current_year()} git workshop!")
+    #print(f"The first 10 even integers are: {first_n_even_integers(10)}")
+    #print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
     pass
 
 if __name__ == "__main__":
