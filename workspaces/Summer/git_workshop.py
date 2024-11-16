55ba3c7baad186ddabce0fdfadec93e1dfb3d2d5
Samar <130741241+SUMMERxKx@users.noreply.github.com>
2024-11-16 14:29:41 -0800

    Fix the conflict

--- workspaces/Summer/git_workshop.py
+++ workspaces/Summer/git_workshop.py
@@ -1,20 +1,18 @@
-# Edit this file
-
-# should return [1,1,2,3,5... and so on
+# should return [1, 1, 2, 3, 5... and so on
 # def first_n_fibonacci(n):
 #     pass
 
 # if n=5, should return 2,4,6,8,10
 # def first_n_even_integers(n):
 #    pass
+
 def current_year():
-     return 2024
+    return 2024
 
 def main():
     print(f"Welcome to the {current_year()} git workshop!")
-    #print(f"The first 10 even integers are: {first_n_even_integers(10)}")
-    #print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
-    pass
+    # print(f"The first 10 even integers are: {first_n_even_integers(10)}")
+    # print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
 
 if __name__ == "__main__":
     main()
