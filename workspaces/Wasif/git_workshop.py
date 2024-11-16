023e7fdfe85b5abff34497ccd6d33ff052458681
Wasif Raiyan <raiyanw22@mytru.ca>
2024-11-16 13:35:45 -0800

    Edited
    Changed year

--- workspaces/Wasif/git_workshop.py
+++ workspaces/Wasif/git_workshop.py
@@ -1,21 +1,22 @@
 # Edit this file
 
 # should return [1,1,2,3,5... and so on
-# def first_n_fibonacci(n):
+#     def first_n_fibonacci(n):
 #     pass
 
-# if n=5, should return 2,4,6,8,10
-# def first_n_even_integers(n):
-#    pass
+#  if n=5, should return 2,4,6,8,10
+#     def first_n_even_integers(n):
+#     pass
 
-# def current_year_workshop():
-#     return 2023
+
+
+def current_year_workshop():
+    return 2024
 
 def main():
-    # print(f"Welcome to the {current_year_workshop()} git workshop!")
+    print(f"Welcome to the {current_year_workshop()} git workshop!")
     # print(f"The first 10 even integers are: {first_n_even_integers(10)}")
-    # print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")
-    pass
+    # print(f"The first 10 even fibonacci are: {first_n_fibonacci(10)}")s
 
 if __name__ == "__main__":
     main()
